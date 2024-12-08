3d1532afd254144af731bb4c521b7287bfd8c404
nesaa-a <agnesahalimii@outlook.com>
2024-12-08 19:40:59 +0100

    Created the Man Section

--- man.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.product').forEach(product => {
-    product.addEventListener('click', () => {
-      alert('Product clicked: ' + product.querySelector('h2').innerText);
-    });
-  });
-  
